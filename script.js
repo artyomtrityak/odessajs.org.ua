639a2ffe73474d0158a35c5194c6ceb0a6280bd4
Arten Savienkov <savienkov.ao@gmail.com>
2018-06-22 21:39:40 +0300

    socials fix

--- script.js
+++ script.js
@@ -219,12 +219,6 @@ function loadAskQuestionModal($speakerBlock) {
   }
 //  Image Lazy loading
 
-  /* Cloud tag */
-  $('.tagBall').cloudTag({
-    ballSize:200 // the size of the tag cloud ball
-  });
-  /* Cloud tag */
-
 })(jQuery); // End of use strict
 
 
